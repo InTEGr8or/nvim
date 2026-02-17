daaeaf8f40ab9cd004759ecae544cf98e971c1b1
Mark Stouffer <1802850+InTEGr8or@users.noreply.github.com>
2026-02-16 19:03:36 -0800

    feat(neotree): force sibling navigation mappings and disable conflicts

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -14,15 +14,25 @@ return {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
+    window = {
+      mappings = {
+        ['\\'] = 'close_window',
+        ['J'] = 'next_sibling',
+        ['K'] = 'prev_sibling',
+        ['j'] = 'none',
+        ['k'] = 'none',
+        ['h'] = function(state)
+          local node = state.tree:get_node()
+          if node and node.level > 0 then
+            require('neo-tree.ui.renderer').focus_node(state, node:get_parent_id())
+          end
+        end,
+      },
+    },
     filesystem = {
       filtered_items = {
         visible = true,
         hide_git_ignored = true,
-      },
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-        },
       },
     },
   },
