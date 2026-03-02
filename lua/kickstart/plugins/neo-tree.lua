a8f2f015ea3a0f07480d2217c2360aaab8297601
Mark Stouffer <1802850+InTEGr8or@users.noreply.github.com>
2026-03-02 12:09:36 -0800

    feat(neotree): add 'Y' mapping and :CopyRelPath command

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -13,7 +13,30 @@ return {
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
+  init = function()
+    vim.api.nvim_create_user_command('CopyRelPath', function()
+      local state = require('neo-tree.sources.manager').get_state 'filesystem'
+      local node = state.tree:get_node()
+      if node then
+        local path = node:get_id()
+        local relpath = vim.fn.fnamemodify(path, ':.')
+        vim.fn.setreg('+', relpath)
+        vim.notify('Copied relative path: ' .. relpath)
+      else
+        vim.notify('No node selected in Neo-tree', vim.log.levels.WARN)
+      end
+    end, { desc = 'Copy relative path of current Neo-tree node' })
+  end,
   opts = {
+    commands = {
+      copy_path = function(state)
+        local node = state.tree:get_node()
+        local path = node:get_id()
+        local relpath = vim.fn.fnamemodify(path, ':.')
+        vim.fn.setreg('+', relpath)
+        vim.notify('Copied relative path: ' .. relpath)
+      end,
+    },
     window = {
       mappings = {
         ['\\'] = 'close_window',
@@ -21,13 +44,7 @@ return {
         ['K'] = 'prev_sibling',
         ['j'] = 'none',
         ['k'] = 'none',
-        ['Y'] = function(state)
-          local node = state.tree:get_node()
-          local path = node:get_id()
-          local relpath = vim.fn.fnamemodify(path, ':.')
-          vim.fn.setreg('+', relpath)
-          vim.notify('Copied relative path: ' .. relpath)
-        end,
+        ['Y'] = 'copy_path',
         ['<leader>gv'] = function() vim.cmd('DiffviewOpen') end,
         ['<leader>gh'] = function() vim.cmd('DiffviewFileHistory') end,
         ['<leader>gf'] = function(state)
