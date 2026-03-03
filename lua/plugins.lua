6ba3fd77d042c07646c3baccb50c2f33ecdbef6e
Mark Stouffer <1802850+InTEGr8or@users.noreply.github.com>
2026-03-02 18:20:17 -0800

    fix(telescope): correct zoxide extension configuration

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -223,6 +223,25 @@ return {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
           },
+          zoxide = {
+            mappings = {
+              default = {
+                after_action = function(selection)
+                  local path = selection.path
+                  vim.cmd('cd ' .. vim.fn.fnameescape(path))
+                  -- Trigger Neo-tree to root itself in the new directory
+                  local ok_nt, nt_manager = pcall(require, 'neo-tree.sources.manager')
+                  if ok_nt then
+                    local state = nt_manager.get_state 'filesystem'
+                    if state then
+                      nt_manager.navigate(state, path)
+                    end
+                  end
+                  vim.notify('Changed root to: ' .. path)
+                end,
+              },
+            },
+          },
         },
       }
 
@@ -241,27 +260,7 @@ return {
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-      vim.keymap.set('n', '<leader>sz', function()
-        require('telescope').extensions.zoxide.list {
-          mappings = {
-            default = {
-              after_action = function(selection)
-                local path = selection.path
-                vim.cmd('cd ' .. vim.fn.fnameescape(path))
-                -- Trigger Neo-tree to root itself in the new directory
-                local ok_nt, nt_manager = pcall(require, 'neo-tree.sources.manager')
-                if ok_nt then
-                  local state = nt_manager.get_state 'filesystem'
-                  if state then
-                    nt_manager.navigate(state, path)
-                  end
-                end
-                vim.notify('Changed root to: ' .. path)
-              end,
-            },
-          },
-        }
-      end, { desc = '[S]earch [Z]oxide (Change Root)' })
+      vim.keymap.set('n', '<leader>sz', require('telescope').extensions.zoxide.list, { desc = '[S]earch [Z]oxide (Change Root)' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
