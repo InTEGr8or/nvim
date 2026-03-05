974da97df8054c34b8250d3b4f1f6bc7ccf22cd7
Mark Stouffer <1802850+InTEGr8or@users.noreply.github.com>
2026-03-04 23:14:49 -0800

    fix(neotree): use FileType autocmd for robust spacebar and 'u' mappings

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -14,6 +14,7 @@ return {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   init = function()
+    -- Global command for copying relative path
     vim.api.nvim_create_user_command('CopyRelPath', function()
       local state = require('neo-tree.sources.manager').get_state 'filesystem'
       local node = state.tree:get_node()
@@ -26,6 +27,25 @@ return {
         vim.notify('No node selected in Neo-tree', vim.log.levels.WARN)
       end
     end, { desc = 'Copy relative path of current Neo-tree node' })
+
+    -- Aggressive keymap overrides for Neo-tree
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = 'neo-tree',
+      callback = function()
+        -- Force space to be the leader key (unmap Neo-tree default)
+        vim.keymap.set('n', '<space>', '<space>', { remap = true, buffer = true })
+
+        -- Custom 'u' to go up a directory
+        vim.keymap.set('n', 'u', function()
+          local state = require('neo-tree.sources.manager').get_state 'filesystem'
+          local path = state.path
+          local parent = vim.fn.fnamemodify(path, ':h')
+          vim.cmd('cd ' .. vim.fn.fnameescape(parent))
+          require('neo-tree.sources.manager').navigate(state, parent)
+          vim.notify('Changed root to: ' .. parent)
+        end, { buffer = true, desc = 'Go up to parent directory' })
+      end,
+    })
   end,
   opts = {
     commands = {
@@ -55,7 +75,6 @@ return {
             vim.notify('Cannot change root to a file', vim.log.levels.WARN)
           end
         end,
-        ['<space>'] = 'nop',
         ['C'] = function(state)
           local node = state.tree:get_node()
           if node.type == 'directory' then
