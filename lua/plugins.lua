fc769a2ddbc838f2fdc8c390edea5fab4b7b69d7
Mark Stouffer <1802850+InTEGr8or@users.noreply.github.com>
2026-04-06 21:19:21 -0700

    Add and complete tasks

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1152,6 +1152,8 @@ return {
   require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
+  { 'will133/vim-dirdiff', cmd = { 'DirDiff' } },
+
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
